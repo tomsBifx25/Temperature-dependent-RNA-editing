38fd5703b9b12dab5f35d1665a001df24bd8f187
tomsBifx25 <tomjw31@gmail.com>
2026-09-17 15:05:53 -0400

    update multiqc to run with temp-depend-rna samples

--- run_multiqc.sh
+++ run_multiqc.sh
@@ -8,6 +8,6 @@
 echo "Aggregating reports with MultiQC..."
 
 # Run multiqc on the results directory and output the HTML report into results/
-multiqc results/ -o results/
+multiqc $HOME/BIFX_572/temp2-Noah/results/ -o $HOME/BIFX_572/temp2-Noah/results/
 
 echo "MultiQC complete!"
