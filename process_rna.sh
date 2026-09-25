339d51ead951c195f7bfb8afe28b50775b4d293a
tomsBifx25 <tomjw31@gmail.com>
2026-09-24 21:30:18 -0400

    made 2 files and edits to process.rna.sh
    created Salmon2R.R from importBioc2R and made changes based on class discussions, created samples metadata as a placeholder for the appropriate csv file. and for process_rna, altered the index to include the  full transcriptome index. STILL NEED TO CHANGE THE PATHS

--- process_rna.sh
+++ process_rna.sh
@@ -4,9 +4,20 @@
 # for example, to run ERR458494, that should be the only sampe in the file
 
 # to run; bash process_rna.sh
+# also, think about running in background w/nohup so you can walk away/do something else.
+# use this cmd: nohup bash scripts/process-rna.sh > process.log 2>&1 &
+#     this will run it with (no hangups) and give a progress log file you can check.
+
+# Check if it's still running: ps aux | grep process-rna.sh
+# View real-time progress: tail -f process.log
+# (Press Ctrl + C to exit the live view without stopping the script).
+# Safely kill the job if something goes wrong:
+#      Find the Process ID (PID) from the ps command above and run kill <PID>.
+
+########### Change paths from hardcoded to symbolic links to the right locations
 
 # Make sure this is your index directory
-INDEX="/mnt/cluster_storage/data/ref/Octopus_bimaculoides_index"
+INDEX="/mnt/cluster_storage/data/ref/Octopus_bimaculoides_2_ASM119413v2_index"
 
 
 # Get an array of all samples to be processed
